99734798ab2b892dbdf2e1ec802f0a0753936e6d
Vineeth -2443 <vineeth.macharla@nutanix.com>
2021-09-05 11:41:28 +0530

    commit

--- physicalmemory.py
+++ physicalmemory.py
@@ -1,16 +1,13 @@
 import psutil
-# gives a single float value
-psutil.cpu_percent(10)
+
+psutil.cpu_percent(5)
 print('The CPU usage is: ', psutil.cpu_percent(4))
-# gives an object with many fields
+
 psutil.virtual_memory()
 print(psutil.virtual_memory())
-# you can convert that object to a dictionary
-# dict(psutil.virtual_memory()._asdict())
-# print(dict(psutil.virtual_memory()._asdict()))
-# you can have the percentage of used RAM
+
 psutil.virtual_memory().percent
 print(psutil.virtual_memory().percent)
-# you can calculate percentage of available memory
+
 psutil.virtual_memory().available * 100 / psutil.virtual_memory().total
 print(psutil.virtual_memory().available * 100 / psutil.virtual_memory().total)
