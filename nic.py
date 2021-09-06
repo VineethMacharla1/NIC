0670750e80e3d257a75fbe16118376c076ab4b13
Vineeth -2443 <vineeth.macharla@nutanix.com>
2021-09-06 13:06:44 +0530

    commitS1

--- nic.py
+++ nic.py
@@ -1,24 +1,48 @@
 import subprocess
+import platform
 import re
 
-cmd = "wmic nic get AdapterType, Name, Installed, MACAddress, GUID, Manufacturer, Availability, NetConnectionID, PowerManagementSupported, Speed."
-#cmd = "ipconfig"
-p1 = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-out, err = p1.communicate()
+if platform.system() == "Windows":
 
-print('out: {0}'.format(out))
-print('error : {0}'.format(err))
+    cmd = "wmic nic get AdapterType, Name, Installed, MACAddress, GUID, Manufacturer, Availability, NetConnectionID, PowerManagementSupported, Speed."
 
-if p1.returncode == 0:
-    print('command : success')
-else:
-    print('command : failed')
+    B = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    out, err = B.communicate()
+    print('out:Hardware,Device and Ethernet Address {0}'.format(out))
+
+    if B.returncode == 0:
+        print('Result : Passed')
+    else:
+        print("Result : Failed")
+        print("err:couldn't find{0}".format(err))
+
+elif platform.system() == "Darwin":
+
+    cmd = "networksetup -listallhardwareports"
+
+    B = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    out, err = B.communicate()
+    print('out:Hardware,Device and Ethernet Address {0}'.format(out))
+
+    if B.returncode == 0:
+        print('Result : Passed')
+    else:
+        print("Result : Failed")
+        print("err:couldn't find{0}".format(err))
+
+elif platform.system() == "Linux":
+
+    cmd = "hwinfo"
+
+    B = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+    out, err = B.communicate()
+    print('out:Hardware,Device and Ethernet Address {0}'.format(out))
+
+    if B.returncode == 0:
+        print('Result : Passed')
+    else:
+        print("Result : Failed")
+        print("err:couldn't find{0}".format(err))
 
 
-with open('text.txt', 'r') as file:
-    filedata = file.read()
 
-ver=re.sub("VERSION:1.1","VERSION:1.2",filedata)
-
-with open('text.txt', 'w') as file:
-    file.write(ver)
